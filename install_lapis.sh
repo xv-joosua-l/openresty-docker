ab82e6fe55900705814364c1b99619cf2dd1780c
Aliaksandr Rahalevich <saksmlz@gmail.com>
2015-12-01 15:14:39 +0800

    upgrade openresty to 1.9.3.2 version

--- install_lapis.sh
+++ install_lapis.sh
@@ -4,7 +4,7 @@ wget http://luarocks.org/releases/luarocks-${LUAROCKS_VERSION}.tar.gz && \
   ./configure \
     --prefix=/opt/openresty/luajit/ \
     --with-lua=/opt/openresty/luajit/ \
-    --lua-suffix=jit-2.1.0-alpha \
+    --lua-suffix=jit-2.1.0-beta1 \
     --with-lua-include=/opt/openresty/luajit/include/luajit-2.1/ && \
   make build && \
   make install && \
